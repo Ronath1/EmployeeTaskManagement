75fa5533aeb42eae52cf7f31fcd84c206a80c380
Ronath1 <140684168+Ronath1@users.noreply.github.com>
2026-08-31 20:23:36 +0530

    updates on teh backend

--- EmployeeTaskManagement.API/Program.cs
+++ EmployeeTaskManagement.API/Program.cs
@@ -66,6 +66,15 @@ builder.Services.AddSwaggerGen(options =>
         }
     });
 });
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ReactClient", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -81,6 +90,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("ReactClient");
 
 app.UseAuthentication();
 
